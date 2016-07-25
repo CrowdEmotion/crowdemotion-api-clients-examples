2e8ca0089388c5118fa7efac2d80f676bdd95e95
Ignacio Peluffo <ipeluffo@gmail.com>
2016-07-25 14:43:26 +0100

    [#126378485] Java example added

---
+++ java/src/main/java/co/uk/crowdemotion/JavaClientExampleMain.java
@@ -0,0 +1,47 @@
+package co.uk.crowdemotion;
+
+import io.swagger.client.ApiClient;
+import io.swagger.client.ApiException;
+import io.swagger.client.api.ResearchApi;
+import io.swagger.client.model.Research;
+
+import java.util.List;
+
+/**
+ * Created by ipeluffo on 7/21/16.
+ */
+public class JavaClientExampleMain {
+
+    private static final String API_TOKEN = "YOUR_USER_API_TOKEN";
+
+    public static void main(String[] args) {
+        ApiClient apiClient = new ApiClient();
+
+        apiClient.addDefaultHeader("Authorization", API_TOKEN);
+
+//         It is not supported in the current Swagger implementation yet
+//         apiClient.setApiKey(API_TOKEN);
+
+        System.out.println("API BASE PATH: " + apiClient.getBasePath());
+
+        for (String key : apiClient.getAuthentications().keySet()) {
+            System.out.println("Authentication "+key+": "+apiClient.getAuthentications().get(key).toString());
+        }
+
+        ResearchApi researchApi = new ResearchApi(apiClient);
+
+        try {
+            List<Research> researchList = researchApi.researchGet(null, 10, null, null);
+
+            System.out.println(researchList.size() + " research found");
+            for (Research research : researchList) {
+                System.out.println(research.toString());
+            }
+        } catch (ApiException exception) {
+            System.out.println("EXCEPTION!: " + exception.getMessage());
+            System.out.println("Response code: "+exception.getCode());
+            System.out.println("Response body: "+exception.getResponseBody());
+        }
+    }
+
+}
