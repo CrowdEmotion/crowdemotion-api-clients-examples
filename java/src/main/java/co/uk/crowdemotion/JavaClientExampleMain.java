bb389afc8a8d59416645ff56359ff3dc27d28a14
Ignacio Peluffo <ipeluffo@gmail.com>
2016-08-11 16:14:02 +0100

    [#128073019] Java Client updated to last release. Java Example updated with upload facevideo example.

--- java/src/main/java/co/uk/crowdemotion/JavaClientExampleMain.java
+++ java/src/main/java/co/uk/crowdemotion/JavaClientExampleMain.java
@@ -1,7 +1,10 @@
 package co.uk.crowdemotion;
 
+import io.swagger.client.model.FaceVideo;
 import io.swagger.client.model.Research;
+import io.swagger.client.model.Timeseries;
 
+import java.io.File;
 import java.util.List;
 
 /**
@@ -31,10 +34,63 @@ public class JavaClientExampleMain {
                 System.out.println(research.toString());
             }
         } catch (ApiException exception) {
-            System.out.println("EXCEPTION!: " + exception.getMessage());
-            System.out.println("Response code: "+exception.getCode());
-            System.out.println("Response body: "+exception.getResponseBody());
+            printException(exception);
         }
+
+        //////////////////////////////////////////////////////////////////////////////////////////
+
+        System.out.println("\n\n>>> FaceVideo upload example: \n");
+
+        FaceVideoApi faceVideoApi = new FaceVideoApi(apiClient);
+        FaceVideo faceVideo = null;
+        try {
+            String facevideoPath = "path/to/video.mp4";
+            faceVideo = faceVideoApi.facevideoPost(new File(facevideoPath), true, null, null, null, null, true);
+            System.out.println(faceVideo);
+        } catch (ApiException exception) {
+            printException(exception);
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////////////
+
+        System.out.println("\n\n>>> Get auto-generated random time-series (since the 'sandbox' parameter was sent as True): \n");
+        if (faceVideo != null) {
+            try {
+                TimeseriesApi timeseriesApi = new TimeseriesApi(apiClient);
+                List<Timeseries> timeseries = null;
+                boolean tsFound = false;
+                while (!tsFound) {
+                    timeseries = timeseriesApi.timeseriesGet(Integer.parseInt(faceVideo.getResponseId()), null, null, null);
+                    if (timeseries.size() < 1) {
+                        System.out.println("The random timeseries is not ready yet. Wait 10 seconds and retry...");
+                        Thread.sleep(10000);
+                    } else {
+                        tsFound = true;
+                    }
+                }
+
+                System.out.println("response_id: " + timeseries.get(0).getResponseId());
+                System.out.println("metric_id: " + timeseries.get(0).getMetricId());
+                System.out.println("metric_name: " + timeseries.get(0).getMetricName());
+                System.out.println("start_index: " + timeseries.get(0).getStartIndex());
+                System.out.println("end_index: " + timeseries.get(0).getEndIndex());
+                System.out.println("step_size: " + timeseries.get(0).getStepSize());
+                System.out.println("custom_message: " + timeseries.get(0).getCustomMessage());
+                System.out.println("data amount: " + timeseries.get(0).getData().size());
+            } catch (ApiException e) {
+                printException(e);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("The FaceVideo upload didn't finish successfully, then skip the search of the Time-series");
+        }
+    }
+
+    private static void printException(ApiException exception) {
+        System.out.println("EXCEPTION!: " + exception.getMessage());
+        System.out.println("Response code: "+exception.getCode());
+        System.out.println("Response body: "+exception.getResponseBody());
     }
 
 }
